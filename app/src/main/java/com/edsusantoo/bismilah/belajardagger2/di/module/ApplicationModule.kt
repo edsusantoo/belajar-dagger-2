dae98f1290d0460d12e5e51aeb4027883904d1c0
Edi Susanto <edsusantoo@gmail.com>
2019-03-26 11:49:59 +0700

    add application module

---
+++ app/src/main/java/com/edsusantoo/bismilah/belajardagger2/di/module/ApplicationModule.kt
@@ -0,0 +1,19 @@
+package com.edsusantoo.bismilah.belajardagger2.di.module
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class ApplicationModule {
+
+    @Provides
+    @Singleton
+    internal fun provideApplication(application: Application): Application = application
+
+    @Provides
+    @Singleton
+    internal fun provideContext(application: Application): Context = application
+}
